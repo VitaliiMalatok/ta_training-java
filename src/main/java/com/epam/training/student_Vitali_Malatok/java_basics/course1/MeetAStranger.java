6f1844c9c0ffb3b3f3d7effb7a2d31b0977ae155
Vitali Molotok <molotok.vitali@gmail.com>
2023-06-13 23:42:53 +0200

    added task 2 in the first topic

---
+++ src/main/java/com/epam/training/student_Vitali_Malatok/java_basics/course1/MeetAStranger.java
@@ -0,0 +1,11 @@
+package com.epam.training.student_Vitali_Malatok.java_basics.course1;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Hello, "+input);
+    }
+}
