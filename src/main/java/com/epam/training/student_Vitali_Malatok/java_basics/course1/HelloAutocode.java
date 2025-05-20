fad7f76360384d35fea58370bb3e50d4a3ed6458
Vitali Molotok <52930680+VitaliiMalatok@users.noreply.github.com>
2025-05-20 16:32:01 +0200

    Update HelloAutocode.java

--- src/main/java/com/epam/training/student_Vitali_Malatok/java_basics/course1/HelloAutocode.java
+++ src/main/java/com/epam/training/student_Vitali_Malatok/java_basics/course1/HelloAutocode.java
@@ -2,6 +2,6 @@ package com.epam.training.student_Vitali_Malatok.java_basics.course1;
 
 public class HelloAutocode {
     public static void main(String[] args) {
-        System.out.println("Hello, Autocode!");
+        System.out.println("Hello, Autocode! ");
     }
 }
